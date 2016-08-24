d00c2c66831e58aed46195c905fd8582b1bce8e7
Adam Kay <u0988616@ad.utah.edu>
2016-08-24 15:59:05 -0600

    Added an Animation class for when we have animations

---
+++ Visual Studio/TruckGame/TruckGame/Animation.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TruckGame
+{
+    class Animation
+    {
+        Texture2D spriteStrip;
+        float scale;
+        int elapsedTime;
+        int frameTime;
+        int frameCount;
+        int currentFrame;
+
+        Color color;
+        Rectangle sourceRect = new Rectangle();
+        Rectangle destinationRect = new Rectangle();
+        public int FrameWidth;
+        public int FrameHeight;
+        public bool Active;
+        public bool Looping;
+        public Vector2 Position;
+
+        public void Initialize(Texture2D texture, Vector2 position, int frameWidth, int frameHeight, int frameCount, int frametime, Color color, float scale, bool looping)
+        {
+            // Keep a local copy of the values passed in
+
+            this.color = color;
+
+            this.FrameWidth = frameWidth;
+            this.FrameHeight = frameHeight;
+
+            this.frameCount = frameCount;
+            this.frameTime = frametime;
+            this.scale = scale;
+
+            Looping = looping;
+            Position = position;
+            spriteStrip = texture;
+
+            // Set the time to zero
+            elapsedTime = 0;
+            currentFrame = 0;
+
+            // Set the Animation to active by default
+            Active = true;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            // Do not update the game if we are not active
+            if (Active == false) return;
+
+            // Update the elapsed time
+            elapsedTime += (int)gameTime.ElapsedGameTime.TotalMilliseconds;
+
+            // If the elapsed time is larger than the frame time
+            // we need to switch frames
+            if (elapsedTime > frameTime)
+            {
+                // Move to the next frame
+                currentFrame++;
+
+                // If the currentFrame is equal to frameCount reset currentFrame to zero
+                if (currentFrame == frameCount)
+                {
+                    currentFrame = 0;
+
+                    // If we are not looping deactivate the animation
+                    if (Looping == false)
+                        Active = false;
+                }
+
+                // Reset the elapsed time to zero
+                elapsedTime = 0;
+
+            }
+
+            // Grab the correct frame in the image strip by multiplying the currentFrame index by the Frame width
+            sourceRect = new Rectangle(currentFrame * FrameWidth, 0, FrameWidth, FrameHeight);
+
+            // Grab the correct frame in the image strip by multiplying the currentFrame index by the frame width
+            destinationRect.X = (int)(Position.X - (FrameWidth * scale) / 2);
+            destinationRect.Y = (int)(Position.Y - (FrameHeight * scale) / 2);
+            destinationRect.Width = (int)(FrameWidth * scale);
+            destinationRect.Height = (int)(FrameHeight * scale);
+        }
+
+        // Draw the Animation Strip
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            // Only draw the animation when we are active
+            if (Active)
+            {
+                spriteBatch.Draw(spriteStrip, destinationRect, sourceRect, color);
+            }
+        }
+
+    }
+}
