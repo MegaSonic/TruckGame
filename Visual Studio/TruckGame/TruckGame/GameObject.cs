2d76a20f4a2583a2b05e66e4085fe7fe283b2951
Adam Kay <u0988616@ad.utah.edu>
2016-08-25 09:52:39 -0600

    Added GameObject and Sprite classes

---
+++ Visual Studio/TruckGame/TruckGame/GameObject.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TruckGame
+{
+    class GameObject
+    {
+        
+        public Vector2 Position;
+        public float rotation = 0.0f;
+        public float depth = 1.0f;
+        public float scale = 1.0f;
+
+
+        public GameObject(Vector2 position)
+        {
+            this.Position = position;
+        }
+
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+            // spriteBatch.Draw(texture, Position, null, Color.White, rotation, new Vector2(texture.Width / 2, texture.Height / 2), scale, SpriteEffects.None, depth);
+        }
+    }
+}
---
+++ Visual Studio/TruckGame/TruckGame/Sprite.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TruckGame 
+{
+    class Sprite : GameObject
+    {
+        Texture2D texture;
+
+        public Sprite(Texture2D _texture, Vector2 _position) : base(_position)
+        {
+            texture = _texture;
+        }
+
+        public Rectangle BoundingBox
+        {
+            get
+            {
+                return new Rectangle(
+                    (int)Position.X - texture.Width / 2,
+                    (int)Position.Y - texture.Height / 2,
+                    texture.Width,
+                    texture.Height);
+            }
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, Position, null, Color.White, rotation, new Vector2(texture.Width / 2, texture.Height / 2), scale, SpriteEffects.None, depth);
+        }
+    }
+}
