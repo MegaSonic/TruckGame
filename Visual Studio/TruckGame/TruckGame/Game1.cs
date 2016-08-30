63759b9784205e21eb74284e6ea640fbdf1d0c39
Adam Kay <u0988616@ad.utah.edu>
2016-08-30 12:37:34 -0600

    Changed resolution

--- Visual Studio/TruckGame/TruckGame/Game1.cs
+++ Visual Studio/TruckGame/TruckGame/Game1.cs
@@ -42,6 +42,8 @@ namespace TruckGame
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
+            graphics.PreferredBackBufferWidth = 1920;
+            graphics.PreferredBackBufferHeight = 1080;
             Content.RootDirectory = "Content";
         }
 
